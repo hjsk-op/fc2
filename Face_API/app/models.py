a760c1202b28c43f202531814db67f7fed8b5d0e
hjsk-op <ino@niceworld.uu.me>
2020-10-05 08:43:15 +0000

    auto

--- Face_API/app/models.py
+++ Face_API/app/models.py
@@ -1,5 +1,27 @@
 from app import db
-from flask import abort
+from flask import abort, json
+
+class User(db.Model):
+    """
+    用户名 UserName
+    """
+    __tablename__="users"
+    #主键
+    uid=db.Column(db.Integer,primary_key=True)
+    #用户名
+    username=db.Column(db.String(50))
+    #密码
+    password=db.Column(db.String(50))
+    #人脸/此处存储人脸图片的链接
+    face=db.Column(db.String(500))
+    def to_json(self):
+        json_blog={
+            'id':self.uid,
+            'username':self.username,
+            'faceURL':self.face
+        }
+        return json.dumps(json_blog)
+    
 
 class Video(db.Model):
     """
@@ -37,4 +59,24 @@ class Video(db.Model):
             'title': self.title,
             'updateTime': self.updateTime
         }
-        return json_video
+        return json_blog
+
+
+def getHomepageData():
+
+    result = {}
+    # 获取banner
+    banners = Video.query.filter_by(theme='banner')
+    result['banner'] = [banner.to_json() for banner in banners]
+    # 获取homepage
+    first = Video.query.filter_by(theme='hot').all()
+    second = Video.query.filter_by(theme='dramatic').all()
+    third = Video.query.filter_by(theme='idol').all()
+    if len(first) and len(second) and len(third):
+        homepage = [{'Hot Broadcast': [item.to_json() for item in first]},
+                    {'Dramatic Theater': [item.to_json() for item in second]},
+                    {'Idol Theatre': [item.to_json() for item in third]}]
+        result['homepage'] = homepage
+        return result
+    else:
+        abort(404)
